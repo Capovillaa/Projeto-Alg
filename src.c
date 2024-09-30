76c48c23c9a043fd2c68c0dd99385c92ade97a46
Henrique Martins <160790963+HenriqueMartins2502@users.noreply.github.com>
2024-09-30 06:59:31 -0300

    Update src.c

--- src.c
+++ src.c
@@ -30,6 +30,53 @@ void cadastro_livro(struct dados_livro estoque[]){
     }    
 }
 
+void consulta_livro(struct dados_livro estoque[]){
+    int optConsulta;
+    printf("Escolha uma opcao de consulta:\n");
+    printf("1 - Listar todos os livros\n");
+    printf("2 - Buscar por ID\n");
+    scanf("%d", &optConsulta);
+
+    if(optConsulta == 1){
+        if(qtdLivros == 0){
+            printf("Nenhum livro cadastrado.\n");
+        }else{
+            for(int i = 0; i < qtdLivros; i++){
+                printf("ID: %d\n", estoque[i].id);
+                printf("Titulo: %s", estoque[i].titulo);
+                printf("Autor: %s", estoque[i].autor);
+                printf("Numero de paginas: %d\n", estoque[i].num_pag);
+                printf("Ano de publicacao: %d\n", estoque[i].ano_pub);
+                printf("Quantidade: %d\n", estoque[i].qtd);
+                printf("-------------------------\n");
+            }
+        }
+    }else if(optConsulta == 2){
+        int id_busca, encontrado = 0;
+        printf("Digite o ID do livro: ");
+        scanf("%d", &id_busca);
+        for(int i = 0; i < qtdLivros; i++){
+            if(estoque[i].id == id_busca){
+                printf("Livro encontrado! \n");
+                printf("ID: %d \n", estoque[i].id);
+                printf("Titulo: %s", estoque[i].titulo);
+                printf("Autor: %s", estoque[i].autor);
+                printf("Numero de paginas: %d \n", estoque[i].num_pag);
+                printf("Ano de publicacao: %d \n", estoque[i].ano_pub);
+                printf("Quantidade: %d \n", estoque[i].qtd);
+                encontrado = 1;
+                break;
+            }
+        }
+        if(!encontrado){
+            printf("Livro com ID %d nao encontrado. \n", id_busca);
+        }
+    }else{
+        printf("Opcao invalida. \n");
+    }
+}
+
+
 int main(){
     struct dados_livro estoque[100];
     int opt = 0;
@@ -41,9 +88,8 @@ int main(){
                 cadastro_livro(estoque);
                 break;
             case 2:
-                
+                consulta_livro(estoque);
+                break; 
         }
     }
-    
-    return 0;
 }
