c5b3a35f0502a1920d7f5ddaa603be0437b636dd
Gabriel Angeli Capovilla <160801041+Capovillaa@users.noreply.github.com>
2024-09-30 18:03:24 -0300

    Update index.c
    ruivo mudei a sua parte coloquei um switch case pra ficar de bigode maximo e no cadastro fiz a verificação do id

--- index.c
+++ index.c
@@ -10,73 +10,86 @@ struct dados_livro{
   int ano_pub;
   int qtd;
 };
-
-void cadastro_livro(struct dados_livro estoque[]){
-    qtdLivros++;
+int verifica_id(struct dados_livro estoque[],int id_temp){
     for(int i = 0;i < qtdLivros;i++){
-        printf("Digite o Id do livro: ");
-        scanf("%d",&estoque[i].id);
-        printf("Digite o titulo do livro: ");
-        fgets(estoque[i].titulo,50,stdin);
-        printf("Digite o nome do autor(a): ");
-        fgets(estoque[i].autor,50,stdin);
-        printf("Digite o numero de paginas: ");
-        scanf("%d",&estoque[i].num_pag);
-        printf("Digite o ano de publicacao: ");
-        scanf("%d",&estoque[i].ano_pub);
-        printf("Digite a quantidade deste livro: ");
-        scanf("%d",&estoque[i].qtd);
-        printf("-------------------------\n");
-    }    
+        if(estoque[i].id == id_temp){
+            return 1;
+        }
+    }
+    return 0;
 }
-
+void cadastro_livro(struct dados_livro estoque[]){
+    int id_temp,qtd_temp;
+    printf("Digite o Id do livro: ");
+    scanf("%d",&id_temp);
+    while(verifica_id(estoque,id_temp)){
+        printf("O Id %d ja existe,Digite outro:\n",id_temp);
+        scanf("%d",&id_temp);
+    }
+    estoque[qtdLivros].id = id_temp;
+    getchar();
+    printf("Digite o titulo do livro: ");
+    fgets(estoque[qtdLivros].titulo,50,stdin);
+    printf("Digite o nome do autor(a): ");
+    fgets(estoque[qtdLivros].autor,50,stdin);
+    printf("Digite o numero de paginas: ");
+    scanf("%d",&estoque[qtdLivros].num_pag);
+    printf("Digite o ano de publicacao: ");
+    scanf("%d",&estoque[qtdLivros].ano_pub);
+    printf("Digite a quantidade deste livro: ");
+    scanf("%d",&estoque[qtdLivros].qtd);
+    printf("-------------------------\n");
+    qtdLivros++;
+}
+void print_livro(struct dados_livro estoque){
+    printf("ID: %d\n", estoque.id);
+    printf("Titulo: %s", estoque.titulo);
+    printf("Autor: %s", estoque.autor);
+    printf("Numero de paginas: %d\n", estoque.num_pag);
+    printf("Ano de publicacao: %d\n", estoque.ano_pub);
+    printf("Quantidade: %d\n", estoque.qtd);
+    printf("-------------------------\n");
+}
 void consulta_livro(struct dados_livro estoque[]){
     int optConsulta;
+    printf("-------------------------\n");
     printf("Escolha uma opcao de consulta:\n");
-    printf("1 - Listar todos os livros\n");
-    printf("2 - Buscar por ID\n");
-    scanf("%d", &optConsulta);
-
-    if(optConsulta == 1){
-        if(qtdLivros == 0){
-            printf("Nenhum livro cadastrado.\n");
-        }else{
-            for(int i = 0; i < qtdLivros; i++){
-                printf("ID: %d\n", estoque[i].id);
-                printf("Titulo: %s", estoque[i].titulo);
-                printf("Autor: %s", estoque[i].autor);
-                printf("Numero de paginas: %d\n", estoque[i].num_pag);
-                printf("Ano de publicacao: %d\n", estoque[i].ano_pub);
-                printf("Quantidade: %d\n", estoque[i].qtd);
-                printf("-------------------------\n");
-            }
-        }
-    }else if(optConsulta == 2){
-        int id_busca, encontrado = 0;
-        printf("Digite o ID do livro: ");
-        scanf("%d", &id_busca);
-        for(int i = 0; i < qtdLivros; i++){
-            if(estoque[i].id == id_busca){
-                printf("Livro encontrado! \n");
-                printf("ID: %d \n", estoque[i].id);
-                printf("Titulo: %s", estoque[i].titulo);
-                printf("Autor: %s", estoque[i].autor);
-                printf("Numero de paginas: %d \n", estoque[i].num_pag);
-                printf("Ano de publicacao: %d \n", estoque[i].ano_pub);
-                printf("Quantidade: %d \n", estoque[i].qtd);
-                encontrado = 1;
+    optConsulta = 0;
+    while(optConsulta != 3){
+        printf("1 - Listar todos os livros\n2 - Buscar por ID\n3 - Sair\n");
+        scanf("%d", &optConsulta);
+        switch (optConsulta){
+            case 1:
+                if(qtdLivros == 0){
+                    printf("Nenhum livro cadastrado.\n");
+                }else{
+                    for(int i = 0; i < qtdLivros; i++){
+                        print_livro(estoque[i]);
+                    }
+                }
                 break;
-            }
+            case 2:
+                int id_busca, encontrado = 0;
+                printf("Digite o ID do livro: ");
+                scanf("%d", &id_busca);
+                for(int i = 0; i < qtdLivros; i++){
+                    if(estoque[i].id == id_busca){
+                        printf("Livro encontrado! \n");
+                        print_livro(estoque[i]);
+                        encontrado = 1;
+                        break;
+                    }
+                }
+                if(!encontrado){
+                    printf("Livro com ID %d nao encontrado. \n", id_busca);
+                }
+            case 3:
+                break;
+            default:
+                printf("Opcao invalida. \n");
         }
-        if(!encontrado){
-            printf("Livro com ID %d nao encontrado. \n", id_busca);
-        }
-    }else{
-        printf("Opcao invalida. \n");
     }
 }
-
-
 int main(){
     struct dados_livro estoque[100];
     int opt = 0;
